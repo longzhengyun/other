61b8e539e2845af1aabd133b57b1757eba05fcaa
Jary <jary0419@163.com>
2016-10-21 01:30:52 +0800

    add gulp demo

---
+++ node/myapp/build/app.js
@@ -0,0 +1,8 @@
+var express = require('express'),
+	http = require('http')
+	app = express();
+http.createServer(function(request, response){
+	response.writeHead(200, {'Content-Type': 'text/plain'});
+	response.end('Hello World\n');
+}).listen(8888);
+console.log('Server running at http://127.0.0.1:8888');
