f009cc93e5ce6f782de9deb6109fab94d421002f
Jary <jary0419@163.com>
2016-11-16 00:31:49 +0800

    app modify

--- node/myapp/app/app.js
+++ node/myapp/app/app.js
@@ -1,8 +1,16 @@
 var express = require('express'),
-	http = require('http')
-	app = express();
+	http = require('http'),
+	fs = require('fs');
+
 http.createServer(function(request, response){
 	response.writeHead(200, {'Content-Type': 'text/plain'});
-	response.end('Hello World\n');
+	fs.readFile('data/test.txt', function(err, data){
+		if(err){
+			return console.error(err);
+		}else{
+			response.end(data);
+		}
+	});
 }).listen(8888);
+
 console.log('Server running at http://127.0.0.1:8888');
