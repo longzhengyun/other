ac652b77d82379608451ae3a975b9523c5f00307
Jary <jary0419@163.com>
2016-12-06 14:35:02 +0800

    prototypeview modify

--- prototypeview/data/prototypefiles.php
+++ prototypeview/data/prototypefiles.php
@@ -8,76 +8,76 @@ $dir_replace = str_replace("\\", "/", "$dir");
 
 //GBK转码
 function detect_encoding($str, $array_encoding){
-	foreach ($array_encoding as $value) {
-		if($str === iconv($value, $value.'//IGNORE', $str)){
-			return $value;
-		}
-	}
+    foreach ($array_encoding as $value) {
+        if($str === iconv($value, $value.'//IGNORE', $str)){
+            return $value;
+        }
+    }
 };
 
 function characet($data){
-	if(!empty($data)){
-		$file_type = detect_encoding($data, array('UTF-8', 'GBK', 'LATIN1', 'BIG5'));
-			if( $file_type != 'UTF-8'){
-				$data = iconv($file_type, "UTF-8//IGNORE", $data);
-			}
-		}
-	return $data;
+    if(!empty($data)){
+        $file_type = detect_encoding($data, array('UTF-8', 'GBK', 'LATIN1', 'BIG5'));
+            if( $file_type != 'UTF-8'){
+                $data = iconv($file_type, "UTF-8//IGNORE", $data);
+            }
+        }
+    return $data;
 };
 
 //获取数据
 function get_allfiles($path, &$files){
-	//如果是目录
-	if(is_dir($path)){
-		$dp = dir($path);
-		while ($file = $dp -> read()) {
-			if($file != "." && $file != ".."){
-				get_allfiles($path."/".$file, $files);
-			}
-		};
-		$dp -> close();
-	};
+    //如果是目录
+    if(is_dir($path)){
+        $dp = dir($path);
+        while ($file = $dp -> read()) {
+            if($file != "." && $file != ".."){
+                get_allfiles($path."/".$file, $files);
+            }
+        };
+        $dp -> close();
+    };
 
-	//如果是文件
-	if(is_file($path)){
-		handler_prototypedata($path, $files);
-	};
+    //如果是文件
+    if(is_file($path)){
+        handler_prototypedata($path, $files);
+    };
 };
 
 function get_prototypedata($dir){
-	$files = array();
-	get_allfiles($dir, $files);
-	print_r($files);
-	return $files;
+    $files = array();
+    get_allfiles($dir, $files);
+    print_r($files);
+    return $files;
 };
 
 //处理数据
 function handler_prototypedata($path, &$files){
-	$file_name = basename($path);
-	//去掉非html, php文件
-	if(strpos($file_name, ".html") || strpos($file_name, ".php")) {
-		$arr_title = file($path);
+    $file_name = basename($path);
+    //去掉非html, php文件
+    if(strpos($file_name, ".html") || strpos($file_name, ".php")) {
+        $arr_title = file($path);
 
-		//输出网页标题
-		if($arr_title){
-			$arr_title = implode('', $arr_title);
-			preg_match_all('/<title>(.*)<\/title>/i', $arr_title, $title);
-			$page_title = characet(implode('', $title[1]));//转码
-			if(!$page_title){
-				$page_title = '无';
-			};
+        //输出网页标题
+        if($arr_title){
+            $arr_title = implode('', $arr_title);
+            preg_match_all('/<title>(.*)<\/title>/i', $arr_title, $title);
+            $page_title = characet(implode('', $title[1]));//转码
+            if(!$page_title){
+                $page_title = '无';
+            };
 
-			//获取文件修改时间
-			$fmt = date("Y-m-d", filemtime($path));
+            //获取文件修改时间
+            $fmt = date("Y-m-d", filemtime($path));
 
-			global $dir_replace;
+            global $dir_replace;
 
-			$dir_path = str_replace($dir_replace, '', dirname($path).'/');
+            $dir_path = str_replace($dir_replace, '', dirname($path).'/');
 
-			//整合数据
-			$files[] = array("title" => $page_title, "name" => $file_name, "date" => $fmt, "dir" => $dir_path);
-		};
-	}
+            //整合数据
+            $files[] = array("title" => $page_title, "name" => $file_name, "date" => $fmt, "dir" => $dir_path);
+        };
+    }
 };
 
 //导出数据
--- prototypeview/scripts/app.js
+++ prototypeview/scripts/app.js
@@ -1,5 +1,5 @@
 //定义app
-var app = angular.module('app', ['ngSanitize', 'ui.select']);
+var app = angular.module('app', ['ngSanitize', 'ui.router', 'ui.select']);
 
 //常数
 app.constant('HTMLFILES_URL', 'data/prototypefiles.php');
@@ -8,90 +8,103 @@ app.constant('ENTER_DATA', 'data/enterdata.json');
 app.constant('PROJECT_URL', 'http://hfdoc.qa.nt.ctripcorp.com/docmapp/getProjects');
 app.constant('BRANCH_URL', 'http://hfdoc.qa.nt.ctripcorp.com/docmapp/getBranches/');
 
+//路由
+app.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/list');
+    $stateProvider.state('list', {
+        url: '/list?dir=',
+        views: {
+            'main': {
+                templateUrl: 'views/main.html',
+                controller: 'mainController'
+            },
+            'content@list': {
+                templateUrl: 'views/content.html',
+                controller: 'contentController'
+            }
+        }
+    });
+}])
+
 //控制器
-app.controller('mainController', ['$scope', '$location', 'dataService', 'HTMLDATA_URL', 'ENTER_DATA', 'PROJECT_URL', 'BRANCH_URL', function($scope, $location, dataService, HTMLDATA_URL, ENTER_DATA, PROJECT_URL, BRANCH_URL) {
-    //设置加载提示状态
-    $scope.hintState = false;
+app.controller('mainController', ['$scope', '$location', '$state', 'dataService', 'HTMLDATA_URL', 'ENTER_DATA', 'PROJECT_URL', 'BRANCH_URL', function($scope, $location, $state, dataService, HTMLDATA_URL, ENTER_DATA, PROJECT_URL, BRANCH_URL) {
+    //设置原型数据为空
+    $scope.basicData = {};
+
+    //当前目录
+    var searchDir = $location.search().dir;
+    if(searchDir == undefined){
+        $scope.dirSelect = '请选择原型目录';
+    }else{
+        $scope.dirSelect = decodeURIComponent(searchDir);
+    };
 
     //获取快捷入口数据
     dataService.getData(ENTER_DATA).success(function(data) {
         $scope.enterData = data;
     });
 
-    //判断是否为本地目录
-    var host = $location.host();
-    if(host == 'hfdoc.qa.lc.ctripcorp.com'){
-        $scope.localData = true;
-    }else if(host == 'hfdoc.qa.nt.ctripcorp.com'){
-        $scope.localData = false;
+    //设置默认为本地数据
+    $scope.localData = true;
 
-        //获取项目数据
-        dataService.getData(PROJECT_URL).success(function(data) {
-            var keyArray = [];
-            angular.forEach(data.allRemotes, function(value, key){
-                keyArray.push(key);
-            });
-            $scope.userProjects = keyArray;
-        });
-
-        //设置默认分支
-        $scope.userBranchs = ['release'];
-        $scope.userBranchs.selected = 'release';
-    };
-
-    //获取URL参数
-    $scope.getUrlData = function(key){
-        var reg = new RegExp('(^|&)' + key + '=([^&]*)(&|$)');
-        var urlData = window.location.search.substr(1).match(reg);
-        
-        if(urlData != null){
-            return unescape(urlData[2]);
-        }else{
-            return null;
-        };
+    //设置默认列表数据
+    $scope.listData = {
+        prototypeData: [],
+        userBranch: 'release'
     };
 
     //获取all files数据
     dataService.getData(HTMLDATA_URL).success(function(data) {
-        $scope.htmlFilesData = data;
+        //原型 data
+        $scope.basicData.data = data;
 
         //目录列表
-        $scope.dirList = [];
-        var hash = {};
-        angular.forEach(data, function(value){
-            if(!hash[value.dir]){//数组去重
-                $scope.dirList.push(value.dir);
+        $scope.basicData.dirList = [], hash = {};
+        $scope.basicData.dirList.selected = $scope.dirSelect;
+
+        angular.forEach($scope.basicData.data, function(value){
+            //数组去重
+            if(!hash[value.dir]){
+                $scope.basicData.dirList.push(value.dir);
                 hash[value.dir] = true;
-            }
+            };
+            //list数据
+            if(value.dir.indexOf($scope.dirSelect) >= 0){
+                $scope.listData.prototypeData.push(value);
+            };
         });
 
-        //检测URL是否包含参数dir 如果带参数 则加载对应数据
-        var dirData = $scope.getUrlData('dir');
-        if(dirData){
-            $scope.dirList.selected = dirData;
-
-            //按目录加载数据
-            $scope.loadData();
-        };
+        //通知列表更新视图
+        $scope.$broadcast('listView', $scope.listData);
     });
 
-    //按目录加载数据
-    $scope.loadData = function(){
-        $scope.prototypeData = [];
-        angular.forEach($scope.htmlFilesData, function(value){
-            if(value.dir.indexOf($scope.dirList.selected) >= 0){
-                $scope.prototypeData.push(value);
-            };
+    //判断是否为服务器数据
+    var host = $location.host();
+    if(host == 'hfdoc.qa.nt.ctripcorp.com'){
+        //获取项目数据
+        dataService.getData(PROJECT_URL).success(function(data) {
+            var keyArray = [];
+
+            angular.forEach(data.allRemotes, function(value, key){
+                keyArray.push(key);
+            });
+
+            $scope.userProjects = keyArray;
+
+            //设置为非本地目录
+            $scope.localData = false;
+
+            //设置默认分支
+            $scope.userBranchs = ['release'];
+            $scope.userBranchs.selected = 'release';
+
+            $scope.listData.userBranch = $scope.userBranchs.selected;
         });
     };
 
     //设置url参数
     $scope.loadDir = function(dir) {
-        //获取选中目录
-        $scope.dirList.selected = dir;
-
-        //设置url参数
-        window.location.search = '?dir=' + $scope.dirList.selected;
+        $state.go('list', {'dir': dir});//跳转到选中目录
     };
 
     //加载分支数据
@@ -114,6 +127,14 @@ app.controller('mainController', ['$scope', '$location', 'dataService', 'HTMLDAT
             handler(v);//执行方法
         };
     };
+}]);
+
+app.controller('contentController', ['$scope', function($scope) {
+    //更新列表视图
+    $scope.$on('listView', function (e, data) {
+        $scope.prototypeData = data.prototypeData;
+        $scope.userBranch = data.userBranch;
+    })
 }]);
 
 //服务
@@ -140,7 +161,7 @@ app.factory('localService', ['$window', function($window) {
 }])
 
 //指令
-app.directive('domDirective', ['$window', '$timeout', 'dataService', 'localService', 'HTMLFILES_URL', 'HTMLDATA_URL', function($window, $timeout, dataService, localService, HTMLFILES_URL, HTMLDATA_URL) {
+app.directive('domDirective', ['$window', '$timeout', '$state', 'dataService', 'localService', 'HTMLFILES_URL', 'HTMLDATA_URL', function($window, $timeout, $state, dataService, localService, HTMLFILES_URL, HTMLDATA_URL) {
     return {
         restrict: 'AE',
         link: function(scope, element, attrs) {
@@ -203,8 +224,10 @@ app.directive('domDirective', ['$window', '$timeout', 'dataService', 'localServi
                     document.body.removeChild(htmlFilesBox);
 
                     //显示提示信息
-                    scope.hintState = true;
-                    scope.hintText = '更新中，请稍后！';
+                    var hintModule = document.createElement('div');
+                        hintModule.className = 'hint-module';
+                        hintModule.innerHTML = '更新中，请稍候！';
+                        document.body.appendChild(hintModule);
 
                     //生成all files数据
                     dataService.getData(HTMLFILES_URL).success(function() {
@@ -214,26 +237,13 @@ app.directive('domDirective', ['$window', '$timeout', 'dataService', 'localServi
                         localService.setLocalData('updateData', localData); //本地存储数据
 
                         //显示提示信息
-                        scope.hintText = '更新数据成功！';
+                        hintModule.innerHTML = '更新数据成功！';
 
-                        //获取all files数据
-                        dataService.getData(HTMLDATA_URL).success(function(data) {
-                            scope.htmlFilesData = data;
-
-                            //目录列表
-                            scope.dirList = [];
-                            var hash = {};
-                            angular.forEach(data, function(value){
-                                if(!hash[value.dir]){//数组去重
-                                    scope.dirList.push(value.dir);
-                                    hash[value.dir] = true;
-                                }
-                            });
-
-                            $timeout(function(){
-                                scope.hintState = false;
-                            }, 1000);
-                        });
+                        //重新渲染页面
+                        $timeout(function(){
+                            $state.go($state.$current, null, { reload: true });
+                            document.body.removeChild(hintModule); //删除节点
+                        }, 1000);
                     });
                 };
 
